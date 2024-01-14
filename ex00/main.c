288a71415948bd9c1b38cc4b6b95799ea28def47
Choong Seng Khye <cseng-kh@u80z01s01.42kl.edu.my>
2024-01-14 11:16:39 +0800

    submit

--- ex00/main.c
+++ ex00/main.c
@@ -6,13 +6,11 @@
 /*   By: cseng-kh <cseng-kh@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 07:32:37 by cseng-kh          #+#    #+#             */
-/*   Updated: 2024/01/13 23:23:23 by choongsk         ###   ########.fr       */
+/*   Updated: 2024/01/14 11:14:06 by cseng-kh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	rush(int x, int y);
-void	ft_putchar(char c);
-void	ft_put_corresponding_char(int x, int y, int xi, int yi);
 
 int	main(void)
 {
